27340b9522d131fa9b0c2b04497bcd3b09f0373f
rjyashmit <rajyashmit01@gmail.com>
2024-12-27 21:53:30 +0530

    Added Project

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
